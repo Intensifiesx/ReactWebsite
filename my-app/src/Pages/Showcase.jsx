d45045a27ace0e01d8c4e5878687b871e636af33
CoMoMi <97932512+CoMoMi@users.noreply.github.com>
2023-04-22 00:07:24 -0700

    dailies edit

--- my-app/src/Pages/Showcase.jsx
+++ my-app/src/Pages/Showcase.jsx
@@ -5,17 +5,23 @@ import Dailies from "./SubCategories/Dailies";
 import Todos from "./SubCategories/Todos";
 import Rewards from "./SubCategories/Rewards";
 import Skull from "../Images/Skull.png";
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 
 
 export default function Main() {
   const [todos, setTodos] = useState(["Todo 1", "Todo 2", "Todo 3"]);
   return (
     <>
+    <Card>
+      <CardBody>
       <PlayerStats />
       <Habits todos={todos} />
       <Dailies />
       <Todos />
       <Rewards />
+      </CardBody>
+    </Card>
+      
       <div className="bottom-text">
         <img src={Skull} alt="Skull" width="50px" height="50px" />© 2023 Made by
         Silvia and Zain
--- my-app/src/Pages/SubCategories/Dailies.js
+++ my-app/src/Pages/SubCategories/Dailies.js
@@ -1,30 +1,64 @@
 import React from "react";
 import "../../color.css";
+import { 
+  Card, 
+  CardHeader, 
+  CardBody, 
+  Heading,
+  Stack,
+  StackDivider,
+  Box,
+  Button,
+  Input,
+  InputRightElement,
+  InputLeftElement,
+  InputGroup,
+  Checkbox,
+ } from '@chakra-ui/react'
+
 
 export default function Dailies() {
   return (
     <section className="sectionShowcase">
-      <div className="dailies textarea-wrap">
-        <h1 className="header-left">Dailies</h1>
-        <div>
-          <input type="text" placeholder="New Daily" />
-          <button className="clean-button">+</button>
-        </div>
-        <div>
-          <div>
-            <label>
-              <input type="checkbox" />
-              Productive work
-            </label>
-          </div>
-          <div>
-            <label>
-              <input type="checkbox" />
-              Read
-            </label>
-          </div>
-        </div>
-      </div>
+      <Card className="dailies textarea-wrap" height='500px'>
+        <CardHeader>
+        <Heading size='lg' height='10' className="header-left" >Dailies</Heading>
+        </CardHeader>
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing='4'>
+            <Box>
+              <InputGroup size='md'>
+              <Input 
+                pr='4.5rem'
+                type="text" 
+                placeholder="New Daily" 
+                width='180px'
+                />
+              <InputRightElement width='4.5'>
+                <Button className="clean-button">+</Button>
+              </InputRightElement>
+              </InputGroup>
+            </Box>
+            <Box>
+                <input type="checkbox" />
+                Productive work
+            </Box>
+            <Box>
+              <label>
+                <input type="checkbox"/>
+                Productive work
+              </label>
+            </Box>
+            <Box>
+              <label>
+                <input type="checkbox" />
+                Read
+              </label>
+            </Box>
+          </Stack>
+        </CardBody>
+
+      </Card>
     </section>
   );
 }
